3a12e9a22b1815388539500ad3bdc816513fdc1a
Thiago Fontanella <thiago.fontanella@hotmail.com>
2021-03-04 21:46:48 -0300

    Commit inicial projeto bytebank

---
+++ index.js
@@ -0,0 +1,25 @@
+class Cliente {
+    nome;
+    cpf;
+    agencia;
+    saldo;
+}
+
+    
+
+// const cliente1Nome = "Ricardo";
+// const cliente1CPF = 11122233309;
+// const cliente1Agencia = 1001;
+// const cliente1Saldo = 0;
+
+// const cliente2Nome = "Alice";
+// const cliente2CPF = 88822233309;
+// const cliente2Agencia = 1001;
+// const cliente2Saldo = 0;
+
+// const cliente3Nome = "Thiago";
+// const cliente3CPF = 77722233309;
+// const cliente3Agencia = 1001;
+// const cliente3Saldo = 0;
+
+console.log(cliente1Nome);
