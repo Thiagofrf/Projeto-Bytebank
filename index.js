b95331fc4b5c9a156ca33fa94071c03710aab323
Thiago Fontanella <thiago.fontanella@hotmail.com>
2021-03-30 19:05:50 -0300

    avançando no estudo de javascript orientado a objeto

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
 class Cliente { // Classe - molde
     nome;
     cpf;
+}
+
+class ContaCorrente{
     agencia;
     saldo;
+
+    sacar(valor){
+        if(this.saldo > valor){
+            this.saldo -= valor;
+        }
+    }
 }
 
 const cliente1 = new Cliente(); //Instancia da minha classe -- Objeto
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
 const cliente2 = new Cliente();
 cliente2.nome = "Ingrid";
 cliente2.cpf = 88822233309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
-
-// const cliente2Nome = "Alice";
-// const cliente2CPF = 88822233309;
-// const cliente2Agencia = 1001;
-// const cliente2Saldo = 0;
-
-// const cliente3Nome = "Thiago";
-// const cliente3CPF = 77722233309;
-// const cliente3Agencia = 1001;
-// const cliente3Saldo = 0;
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.saldo = 0;
+contaCorrenteRicardo.agencia = 1001;
+contaCorrenteRicardo.sacar(50);
 
 console.log(cliente1, cliente2);
